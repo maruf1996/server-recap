0c05238c9d8fa5411dca9ebc6caeab49df1dcabc
maruf1996 <maruf3813@gmail.com>
2023-07-01 14:41:50 +0600

    'final-commit'

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const cors=require('cors')
+app.use(cors())
+
+const port = process.env.PORT||5000
+
+const productCollections=require('./Data/product.json')
+
+app.get('/', (req, res) => {
+  res.send('Now server is running')
+})
+
+app.get('/allProducts',(req,res)=>{
+    res.send(productCollections)
+})
+
+app.get('/product/:id',(req,res)=>{
+    const id=parseInt(req.params.id)
+    const getSingleItem=productCollections?.find(product=>product.id===id);
+    if(!getSingleItem){
+        res.send('product not found')
+    }
+    res.send(getSingleItem)
+})
+
+app.get('/category/:name',(req,res)=>{
+    const name=req.params.name;
+    const getCategory=productCollections.filter(product=>product.category===name);
+    if(!getCategory){
+        res.send('category not found')
+    }
+    res.send(getCategory)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+
+  module.exports = app;
